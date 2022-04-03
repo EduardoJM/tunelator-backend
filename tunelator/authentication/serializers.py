e7aa06c9f3504778384b5aa96ade29af5da56475
Eduardo Oliveira <eduardo_y05@outlook.com>
2022-04-03 11:08:36 -0300

    add basic authentication api

---
+++ tunelator/authentication/serializers.py
@@ -0,0 +1,38 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import update_last_login
+from rest_framework import serializers
+from rest_framework_simplejwt.serializers import (
+    TokenObtainPairSerializer as BaseTokenObtainPairSerializer,
+)
+
+User = get_user_model()
+
+class AuthenticationUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = [
+            "id",
+            "email",
+            "first_name",
+            "last_name",
+            "date_joined",
+            "last_login",
+        ]
+
+class TokenObtainPairSerializer(BaseTokenObtainPairSerializer):
+    def get_user_data(self):
+        user_serializer = AuthenticationUserSerializer(self.user)
+        return user_serializer.data
+
+    def validate(self, attrs):
+        data = super().validate(attrs)
+
+        refresh = self.get_token(self.user)
+
+        data["refresh"] = str(refresh)
+        data["access"] = str(refresh.access_token)
+        data["user"] = self.get_user_data()
+
+        update_last_login(None, self.user)
+
+        return data
--- tunelator/authentication/views.py
+++ tunelator/authentication/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView as BaseTokenObtainPairView,
+    TokenRefreshView as BaseTokenRefreshView,
+)
+from rest_framework_simplejwt.exceptions import InvalidToken, TokenError
+from rest_framework_simplejwt.tokens import AccessToken
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from authentication.serializers import TokenObtainPairSerializer, AuthenticationUserSerializer
 
-# Create your views here.
+class TokenObtainPairView(BaseTokenObtainPairView):
+    def get_serializer_class(self):
+        return TokenObtainPairSerializer
+
+class TokenRefreshView(BaseTokenRefreshView):
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+
+        try:
+            serializer.is_valid(raise_exception=True)
+        except TokenError as e:
+            raise InvalidToken(e.args[0])
+
+        token = AccessToken(serializer.validated_data["access"])
+        auth = JWTAuthentication()
+        user = auth.get_user(token)
+        user_serializer = AuthenticationUserSerializer(
+            instance=user, context={"request": request}
+        )
+
+        data = {**serializer.validated_data, "user": user_serializer.data}
+        return Response(data)
