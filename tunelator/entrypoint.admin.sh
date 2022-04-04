a9aaebbcde117f494ae5e35a89d45ca7dfe08e18
Eduardo Oliveira <eduardo_y05@outlook.com>
2022-04-04 20:08:14 -0300

    fix entrypoints

--- tunelator/entrypoint.admin.sh
+++ tunelator/entrypoint.admin.sh
@@ -2,7 +2,7 @@ while ! nc -z redis 6379 ; do
     echo "Waiting for the Redis Server"
     sleep 3
 done
-while ! nc -z db 3306 ; do
+while ! nc -z db 5432 ; do
     echo "Waiting for the MySQL Server"
     sleep 3
 done
