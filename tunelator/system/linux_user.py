ce04e564d419d6e8a875818093a0e950fdff73f6
Eduardo Oliveira <eduardo_y05@outlook.com>
2022-04-05 22:23:52 -0300

    update linux_user integration

--- tunelator/system/linux_user.py
+++ tunelator/system/linux_user.py
@@ -12,12 +12,10 @@ def user_already_exists(name: str) -> bool:
     except subprocess.CalledProcessError:
         return False
 
-def get_unique_name(identifier: str) -> str:
-    if len(identifier) > 10:
-        identifier = identifier[0:10]
+def get_unique_name() -> str:
     user_name = str(uuid.uuid4()).replace("-", "")
     if user_already_exists(user_name):
-        return get_unique_name(identifier)
+        return get_unique_name()
     return user_name
 
 def create_mail_anonymous_user(user_name: str) -> bool:
