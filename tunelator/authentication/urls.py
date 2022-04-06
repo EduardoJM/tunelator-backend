ece3743e1d4f071a4b66026e6c6f4812fc3403c1
Eduardo Oliveira <eduardo_y05@outlook.com>
2022-04-05 22:41:29 -0300

    fix wrong import

--- tunelator/authentication/urls.py
+++ tunelator/authentication/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from authentication.views import (
     TokenObtainPairView,
     TokenRefreshView,
-    UserCreateSerializer,
+    UserCreateView,
 )
 
 urlpatterns = [
-    path('create/', UserCreateSerializer.as_view(), name="new_user"),
+    path('create/', UserCreateView.as_view(), name="new_user"),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
