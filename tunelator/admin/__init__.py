066d53145e5f463ed480640b2dbc5f8235c9bf22
Eduardo Oliveira <eduardo_y05@outlook.com>
2022-04-07 19:14:40 -0300

    hotfix of settings call

--- tunelator/admin/__init__.py
+++ tunelator/admin/__init__.py
@@ -1,4 +1,5 @@
-from common.celery import app as celery_app
+from common.celery import setup_app
 from common.firebase import setup_firebase
 
+celery_app = setup_app('admin.settings')
 setup_firebase()
