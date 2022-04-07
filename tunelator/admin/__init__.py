d5938ed459c35e07781b76600c14f7585c2e2775
Eduardo Oliveira <eduardo_y05@outlook.com>
2022-04-07 18:47:16 -0300

    begin implementation of firebase integration

--- tunelator/admin/__init__.py
+++ tunelator/admin/__init__.py
@@ -1,1 +1,4 @@
 from common.celery import app as celery_app
+from common.firebase import setup_firebase
+
+setup_firebase()
---
+++ tunelator/common/firebase.py
@@ -0,0 +1,10 @@
+import os
+from pathlib import Path
+import firebase_admin
+
+def setup_firebase():
+    APP_DIR = Path(__file__).resolve().parent.parent
+    credentials = firebase_admin.credentials.Certificate(
+        os.path.join(APP_DIR, "firebase_keys.json")
+    )
+    firebase_admin.initialize_app(credentials)
