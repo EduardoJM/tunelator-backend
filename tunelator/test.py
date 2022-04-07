cf0d543d39c6c7e960b091952b97673ab9fbaa1b
Eduardo Oliveira <eduardo_y05@outlook.com>
2022-04-07 07:46:39 -0300

    remove unused system app

--- tunelator/test.py
+++
@@ -1,9 +0,0 @@
-from system.linux_user import user_already_exists, get_unique_name, create_mail_anonymous_user
-
-print(
-    create_mail_anonymous_user(
-        get_unique_name("inventare")
-    )
-)
-#print(user_already_exists("root"))
-
