3ca6ef0c44b6421031b23cc9a011467e6f030251
Eduardo Oliveira <eduardo_y05@outlook.com>
2022-04-21 18:12:16 -0300

    try to fix payment bug

--- tunelator/common/settings.py
+++ tunelator/common/settings.py
@@ -150,7 +150,7 @@ CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:3000',
     'http://localhost:3000',
     'https://dashboard.tunelator.com.br',
-    'https://f925-179-54-146-220.ngrok.io/',
+    'https://f925-179-54-146-220.ngrok.io',
 ]
 
 #
