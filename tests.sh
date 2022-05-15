404027f9ac2ea78599eefaea0b0237a71d36d0d7
Eduardo Oliveira <eduardo_y05@outlook.com>
2022-05-14 21:49:31 -0300

    ci: try to fix the ci docker-compose

--- tests.sh
+++ tests.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+while ! nc -z localhost 5432 ; do
+    echo "Waiting for the database server"
+    sleep 3
+done
+
 while ! nc -z localhost 8000 ; do
     echo "Waiting for the api server"
     sleep 0.1
