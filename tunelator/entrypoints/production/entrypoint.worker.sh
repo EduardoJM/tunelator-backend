0aa2fb7bebc928de8123a6366da4fe1fab04192b
Eduardo Oliveira <eduardo_y05@outlook.com>
2022-06-05 10:18:16 -0300

    feat(env): begin changing the production environment to remove redis

--- tunelator/entrypoints/production/entrypoint.worker.sh
+++ tunelator/entrypoints/production/entrypoint.worker.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-while ! nc -z redis 6379 ; do
-    echo "Waiting for the Redis Server"
-    sleep 3
-done
 while ! nc -z db 5432 ; do
     echo "Waiting for the MySQL Server"
     sleep 3
