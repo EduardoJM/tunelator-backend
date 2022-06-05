f894c2ea220e2cd25f6073e35cab9564f3ff3aad
Eduardo Oliveira <eduardo_y05@outlook.com>
2022-06-05 10:13:49 -0300

    feat(env): implements an basic django project startup for dev

---
+++ tunelator/entrypoints/development/entrypoint.api.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+while ! nc -z redis 6379 ; do
+    echo "Waiting for the Redis Server"
+    sleep 3
+done
+while ! nc -z db 5432 ; do
+    echo "Waiting for the MySQL Server"
+    sleep 3
+done
+
+python manage.py migrate
+python manage.py runserver 0.0.0.0:8000
