cbc60dc72f673d11ba6e24bafbd6be6520df1e28
Eduardo Oliveira <eduardo_y05@outlook.com>
2022-06-09 21:31:44 -0300

    feat: begin supporting code coverage

---
+++ coverage.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+while ! nc -z localhost 8000 ; do
+    echo "Waiting for the api server"
+    sleep 0.1
+done
+
+docker exec -ti api coverage run --source='.' manage.py test
+docker exec -ti api coverage html --omit migrations
