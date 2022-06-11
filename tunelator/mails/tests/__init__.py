bfbdf1422e59cc034965d1eb619d1f84c5d7ae2c
Eduardo Oliveira <eduardo_y05@outlook.com>
2022-06-11 20:19:26 -0300

    tests: add some tests for mail accounts

---
+++ tunelator/mails/tests/__init__.py
@@ -0,0 +1,1 @@
+from .test_mail_accounts import *
--- tunelator/mails/urls.py
+++ tunelator/mails/urls.py
@@ -7,6 +7,8 @@ mails_router = routers.SimpleRouter()
 mails_router.register("accounts", UserMailViewSet, basename="UserMail")
 mails_router.register("received", UserReceivedMailViewSet, basename="UserReceivedMail")
 
+app_name = 'mails'
+
 urlpatterns = [
     path('', include(mails_router.urls)),
     path('verify/user/', VerifyUserSystemAPIView.as_view()),
