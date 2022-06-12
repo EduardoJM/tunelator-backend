0b00323a77c7e6533c6c767dc36f09514371ee69
Eduardo Oliveira <eduardo_y05@outlook.com>
2022-06-12 12:06:27 -0300

    chore: small update on the test scripts

--- coverage.sh
+++ coverage.sh
@@ -8,3 +8,4 @@ done
 docker exec -ti api coverage run --source='.' manage.py test
 docker exec -ti api coverage json --omit='*migrations*,*/**/admin.py,*tests*,core/asgi.py,core/wsgi.py,manage.py'
 docker exec -ti api coverage html --omit='*migrations*,*/**/admin.py,*tests*,core/asgi.py,core/wsgi.py,manage.py'
+docker exec -ti api coverage report --omit='*migrations*,*/**/admin.py,*tests*,core/asgi.py,core/wsgi.py,manage.py'
