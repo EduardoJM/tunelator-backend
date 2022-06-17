738bd174d6158441534e1f3e067061d8fdbc4761
Eduardo Oliveira <eduardo_y05@outlook.com>
2022-06-17 11:37:31 -0300

    fix: try to fix tests in workflows

--- tunelator/entrypoints/tests/entrypoint.api.sh
+++ tunelator/entrypoints/tests/entrypoint.api.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+while ! nc -z redis 6379 ; do
+    echo "Waiting for the Redis Server"
+    sleep 3
+done
 while ! nc -z db 5432 ; do
     echo "Waiting for the MySQL Server"
     sleep 3
