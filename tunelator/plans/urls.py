75d63b69ef6c163502592a550bc087ddfb904c51
Eduardo Oliveira <eduardo_y05@outlook.com>
2022-06-19 16:15:21 -0300

    feat: add free e-mail accounts count in the active plan route (#37)
    closes #36
    
    * feat: add free e-mails count to active plan API return
    
    * chore: update the coverage badge

--- tunelator/plans/urls.py
+++ tunelator/plans/urls.py
@@ -5,6 +5,8 @@ from plans.viewsets import PlanViewSet
 plans_router = routers.SimpleRouter()
 plans_router.register("", PlanViewSet, basename="Plan")
 
+app_name = 'plans'
+
 urlpatterns = [
     path('', include(plans_router.urls)),
 ]
