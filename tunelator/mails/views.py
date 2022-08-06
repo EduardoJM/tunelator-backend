067838d529781ccc1213c6ffa7109b5e83a70580
Eduardo Oliveira <eduardo_y05@outlook.com>
2022-08-05 21:00:45 -0300

    docs: add documentation for the UserMailViewSet viewset and for the view

--- tunelator/mails/views.py
+++ tunelator/mails/views.py
@@ -2,13 +2,26 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
+from drf_yasg.utils import swagger_auto_schema
 from mails.serializers import UserMailVerifySerializer
 from mails.services import validate_user_name
+from mails.schemas import IndisponibleResponse, DisponibleResponse
 
 class VerifyUserSystemAPIView(APIView):
     permission_classes = [IsAuthenticated]
     
+    @swagger_auto_schema(
+        request_body=UserMailVerifySerializer,
+        responses={
+            status.HTTP_400_BAD_REQUEST: IndisponibleResponse,
+            status.HTTP_200_OK: DisponibleResponse,
+        },
+    )
     def post(self, request):
+        """
+        Proxy for the user system API verify if an username is
+        disponible or no.
+        """
         serializer = UserMailVerifySerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
 
